807666d6380f0e86eb25223e0a5fbdd52c48b18e
rajuramachandra689-spec <rajuramachandra689@gmail.com>
2026-06-18 17:03:25 +0530

    message

--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import { assets } from '../../assets/assets'
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <img className='logo' src={assets.logo} alt="" />
+      <div className='logo-text'>Eat<span>Ec.</span></div>
       <img className='profile' src={assets.profile_image} alt="" />
     </div>
   )
--- backend/seed.js
+++ backend/seed.js
@@ -1,354 +1,289 @@
-import basket_icon from './basket_icon.png'
-import logo from './logo.png'
-import header_img from './header_img.jpg'
-import search_icon from './search_icon.png'
-import menu_1 from './menu_1.png'
-import menu_2 from './menu_2.png'
-import menu_3 from './menu_3.png'
-import menu_4 from './menu_4.png'
-import menu_5 from './menu_5.png'
-import menu_6 from './menu_6.png'
-import menu_7 from './menu_7.png'
-import menu_8 from './menu_8.png'
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import foodModel from "./models/foodModel.js";
+import { connectDB } from "./config/db.js";
 
-import food_1 from './food_1.png'
-import food_2 from './food_2.png'
-import food_3 from './food_3.png'
-import food_4 from './food_4.png'
-import food_5 from './food_5.png'
-import food_6 from './food_6.png'
-import food_7 from './food_7.png'
-import food_8 from './food_8.png'
-import food_9 from './food_9.png'
-import food_10 from './food_10.png'
-import food_11 from './food_11.png'
-import food_12 from './food_12.png'
-import food_13 from './food_13.png'
-import food_14 from './food_14.png'
-import food_15 from './food_15.png'
-import food_16 from './food_16.png'
-import food_17 from './food_17.png'
-import food_18 from './food_18.png'
-import food_19 from './food_19.png'
-import food_20 from './food_20.png'
-import food_21 from './food_21.png'
-import food_22 from './food_22.png'
-import food_23 from './food_23.png'
-import food_24 from './food_24.png'
-import food_25 from './food_25.png'
-import food_26 from './food_26.png'
-import food_27 from './food_27.png'
-import food_28 from './food_28.png'
-import food_29 from './food_29.png'
-import food_30 from './food_30.png'
-import food_31 from './food_31.png'
-import food_32 from './food_32.png'
+dotenv.config();
 
-import add_icon_white from './add_icon_white.png'
-import add_icon_green from './add_icon_green.png'
-import remove_icon_red from './remove_icon_red.png'
-import app_store from './app_store.png'
-import play_store from './play_store.png'
-import linkedin_icon from './linkedin_icon.png'
-import facebook_icon from './facebook_icon.png'
-import twitter_icon from './twitter_icon.png'
-import cross_icon from './cross_icon.png'
-import selector_icon from './selector_icon.png'
-import rating_starts from './rating_starts.png'
-import profile_icon from './profile_icon.png'
-import bag_icon from './bag_icon.png'
-import logout_icon from './logout_icon.png'
-import parcel_icon from './parcel_icon.png'
-import checked from './checked.png'
-import un_checked from './un_checked.png'
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-export const assets = {
-    logo,
-    basket_icon,
-    header_img,
-    search_icon,
-    rating_starts,
-    add_icon_green,
-    add_icon_white,
-    remove_icon_red,
-    app_store,
-    play_store,
-    linkedin_icon,
-    facebook_icon,
-    twitter_icon,
-    cross_icon,
-    selector_icon,
-    profile_icon,
-    logout_icon,
-    bag_icon,
-    parcel_icon,
-    checked,
-    un_checked
-}
-
-export const menu_list = [
+const foodItems = [
     {
-        menu_name: "Salad",
-        menu_image: menu_1
-    },
-    {
-        menu_name: "Rolls",
-        menu_image: menu_2
-    },
-    {
-        menu_name: "Deserts",
-        menu_image: menu_3
-    },
-    {
-        menu_name: "Sandwich",
-        menu_image: menu_4
-    },
-    {
-        menu_name: "Cake",
-        menu_image: menu_5
-    },
-    {
-        menu_name: "Pure Veg",
-        menu_image: menu_6
-    },
-    {
-        menu_name: "Pasta",
-        menu_image: menu_7
-    },
-    {
-        menu_name: "Noodles",
-        menu_image: menu_8
-    }]
-
-export const food_list = [
-    {
-        _id: "1",
         name: "Greek salad",
-        image: food_1,
+        image: "food_1.png",
         price: 12,
         description: "Food provides essential nutrients for overall health and well-being",
         category: "Salad"
     },
     {
-        _id: "2",
         name: "Veg salad",
-        image: food_2,
+        image: "food_2.png",
         price: 18,
         description: "Food provides essential nutrients for overall health and well-being",
         category: "Salad"
-    }, {
-        _id: "3",
+    },
+    {
         name: "Clover Salad",
-        image: food_3,
+        image: "food_3.png",
         price: 16,
         description: "Food provides essential nutrients for overall health and well-being",
         category: "Salad"
-    }, {
-        _id: "4",
+    },
+    {
         name: "Chicken Salad",
-        image: food_4,
+        image: "food_4.png",
         price: 24,
         description: "Food provides essential nutrients for overall health and well-being",
         category: "Salad"
-    }, {
-        _id: "5",
+    },
+    {
         name: "Lasagna Rolls",
-        image: food_5,
+        image: "food_5.png",
         price: 14,
         description: "Food provides essential nutrients for overall health and well-being",
         category: "Rolls"
-    }, {
-        _id: "6",
+    },
+    {
         name: "Peri Peri Rolls",
-        image: food_6,
+        image: "food_6.png",
         price: 12,
         description: "Food provides essential nutrients for overall health and well-being",
         category: "Rolls"
-    }, {
-        _id: "7",
+    },
+    {
         name: "Chicken Rolls",
-        image: food_7,
+        image: "food_7.png",
         price: 20,
         description: "Food provides essential nutrients for overall health and well-being",
         category: "Rolls"
-    }, {
-        _id: "8",
+    },
+    {
         name: "Veg Rolls",
-        image: food_8,
+        image: "food_8.png",
         price: 15,
         description: "Food provides essential nutrients for overall health and well-being",
         category: "Rolls"
-    }, {
-        _id: "9",
+    },
+    {
         name: "Ripple Ice Cream",
-        image: food_9,
+        image: "food_9.png",
         price: 14,
         description: "Food provides essential nutrients for overall health and well-being",
         category: "Deserts"
-    }, {
-        _id: "10",
+    },
+    {
         name: "Fruit Ice Cream",
-        image: food_10,
+        image: "food_10.png",
         price: 22,
         description: "Food provides essential nutrients for overall health and well-being",
         category: "Deserts"
-    }, {
-        _id: "11",
+    },
+    {
         name: "Jar Ice Cream",
-        image: food_11,
+        image: "food_11.png",
         price: 10,
         description: "Food provides essential nutrients for overall health and well-being",
         category: "Deserts"
-    }, {
-        _id: "12",
+    },
+    {
         name: "Vanilla Ice Cream",
-        image: food_12,
+        image: "food_12.png",
         price: 12,
         description: "Food provides essential nutrients for overall health and well-being",
         category: "Deserts"
     },
     {
-        _id: "13",
         name: "Chicken Sandwich",
-        image: food_13,
+        image: "food_13.png",
         price: 12,
         description: "Food provides essential nutrients for overall health and well-being",
         category: "Sandwich"
     },
     {
-        _id: "14",
         name: "Vegan Sandwich",
-        image: food_14,
+        image: "food_14.png",
         price: 18,
         description: "Food provides essential nutrients for overall health and well-being",
         category: "Sandwich"
-    }, {
-        _id: "15",
+    },
+    {
         name: "Grilled Sandwich",
-        image: food_15,
+        image: "food_15.png",
         price: 16,
         description: "Food provides essential nutrients for overall health and well-being",
         category: "Sandwich"
-    }, {
-        _id: "16",
+    },
+    {
         name: "Bread Sandwich",
-        image: food_16,
+        image: "food_16.png",
         price: 24,
         description: "Food provides essential nutrients for overall health and well-being",
         category: "Sandwich"
-    }, {
-        _id: "17",
+    },
+    {
         name: "Cup Cake",
-        image: food_17,
+        image: "food_17.png",
         price: 14,
         description: "Food provides essential nutrients for overall health and well-being",
         category: "Cake"
-    }, {
-        _id: "18",
+    },
+    {
         name: "Vegan Cake",
-        image: food_18,
+        image: "food_18.png",
         price: 12,
         description: "Food provides essential nutrients for overall health and well-being",
         category: "Cake"
-    }, {
-        _id: "19",
+    },
+    {
         name: "Butterscotch Cake",
-        image: food_19,
+        image: "food_19.png",
         price: 20,
         description: "Food provides essential nutrients for overall health and well-being",
         category: "Cake"
-    }, {
-        _id: "20",
+    },
+    {
         name: "Sliced Cake",
-        image: food_20,
+        image: "food_20.png",
         price: 15,
         description: "Food provides essential nutrients for overall health and well-being",
         category: "Cake"
-    }, {
-        _id: "21",
+    },
+    {
         name: "Garlic Mushroom ",
-        image: food_21,
+        image: "food_21.png",
         price: 14,
         description: "Food provides essential nutrients for overall health and well-being",
         category: "Pure Veg"
-    }, {
-        _id: "22",
+    },
+    {
         name: "Fried Cauliflower",
-        image: food_22,
+        image: "food_22.png",
         price: 22,
         description: "Food provides essential nutrients for overall health and well-being",
         category: "Pure Veg"
-    }, {
-        _id: "23",
+    },
+    {
         name: "Mix Veg Pulao",
-        image: food_23,
+        image: "food_23.png",
         price: 10,
         description: "Food provides essential nutrients for overall health and well-being",
         category: "Pure Veg"
-    }, {
-        _id: "24",
+    },
+    {
         name: "Rice Zucchini",
-        image: food_24,
+        image: "food_24.png",
         price: 12,
         description: "Food provides essential nutrients for overall health and well-being",
         category: "Pure Veg"
     },
     {
-        _id: "25",
         name: "Cheese Pasta",
-        image: food_25,
+        image: "food_25.png",
         price: 12,
         description: "Food provides essential nutrients for overall health and well-being",
         category: "Pasta"
     },
     {
-        _id: "26",
         name: "Tomato Pasta",
-        image: food_26,
+        image: "food_26.png",
         price: 18,
         description: "Food provides essential nutrients for overall health and well-being",
         category: "Pasta"
-    }, {
-        _id: "27",
+    },
+    {
         name: "Creamy Pasta",
-        image: food_27,
+        image: "food_27.png",
         price: 16,
         description: "Food provides essential nutrients for overall health and well-being",
         category: "Pasta"
-    }, {
-        _id: "28",
+    },
+    {
         name: "Chicken Pasta",
-        image: food_28,
+        image: "food_28.png",
         price: 24,
         description: "Food provides essential nutrients for overall health and well-being",
         category: "Pasta"
-    }, {
-        _id: "29",
+    },
+    {
         name: "Buttter Noodles",
-        image: food_29,
+        image: "food_29.png",
         price: 14,
         description: "Food provides essential nutrients for overall health and well-being",
         category: "Noodles"
-    }, {
-        _id: "30",
+    },
+    {
         name: "Veg Noodles",
-        image: food_30,
+        image: "food_30.png",
         price: 12,
         description: "Food provides essential nutrients for overall health and well-being",
         category: "Noodles"
-    }, {
-        _id: "31",
+    },
+    {
         name: "Somen Noodles",
-        image: food_31,
+        image: "food_31.png",
         price: 20,
         description: "Food provides essential nutrients for overall health and well-being",
         category: "Noodles"
-    }, {
-        _id: "32",
+    },
+    {
         name: "Cooked Noodles",
-        image: food_32,
+        image: "food_32.png",
         price: 15,
         description: "Food provides essential nutrients for overall health and well-being",
         category: "Noodles"
     }
-]
+];
+
+const seedDatabase = async () => {
+    try {
+        console.log("Connecting to database...");
+        await connectDB();
+
+        // Ensure backend uploads directory exists
+        const uploadsDir = path.join(__dirname, "uploads");
+        if (!fs.existsSync(uploadsDir)) {
+            fs.mkdirSync(uploadsDir, { recursive: true });
+        }
+
+        const assetsDir = path.join(__dirname, "..", "frontend", "src", "assets");
+        console.log("Copying images from frontend/src/assets to backend/uploads...");
+
+        let copiedCount = 0;
+        for (let i = 1; i <= 32; i++) {
+            const imageName = `food_${i}.png`;
+            const sourcePath = path.join(assetsDir, imageName);
+            const destPath = path.join(uploadsDir, imageName);
+
+            if (fs.existsSync(sourcePath)) {
+                fs.copyFileSync(sourcePath, destPath);
+                copiedCount++;
+            } else {
+                console.warn(`Warning: Source image ${sourcePath} not found.`);
+            }
+        }
+        console.log(`Copied ${copiedCount} images successfully.`);
+
+        console.log("Seeding database food items...");
+        let insertedCount = 0;
+        for (const item of foodItems) {
+            const exists = await foodModel.findOne({ name: item.name });
+            if (!exists) {
+                const food = new foodModel(item);
+                await food.save();
+                insertedCount++;
+            }
+        }
+
+        console.log(`Database seeding completed! Successfully inserted ${insertedCount} new food items.`);
+        process.exit(0);
+    } catch (error) {
+        console.error("Error seeding database:", error);
+        process.exit(1);
+    }
+};
+
+seedDatabase();
